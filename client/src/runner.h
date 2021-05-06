2ea89e44ed962bb1be0e0c922a687c2eb14c6456
Jonathan Mace <jonathan.c.mace@gmail.com>
2021-05-06 17:53:02 +0200

    Move runner, impl directly in benchmark. Added threads.

--- client/src/runner.h
+++
@@ -1,8 +0,0 @@
-#ifndef _HINDSIGHT_CLIENT_RUNNER_H_
-#define _HINDSIGHT_CLIENT_RUNNER_H_
-
-/* Used by hindsight test programs to run simple traces */
-
-
-
-#endif // _HINDSIGHT_CLIENT_RUNNER_H_
--- client/test/benchmark.c
+++ client/test/benchmark.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <argp.h>
+#include <pthread.h>
 
 #include "buffer.h"
 #include "tracer.h"
@@ -21,6 +22,7 @@ static struct argp_option options[] = {
   {"buffer_count",  'c', "NUM",  0,  "Number of buffers in pool" },
   {"tracepoints", 'n', "NUM", 0, "Number of tracepoints per trace"},
   {"trigger", 'p', "NUM", 0, "Trigger probability, default 0, float"},
+  {"duration", 'd', "NUM", 0, "Duration in seconds before exiting. 0 to run forever"},
   {"output",   'o', "FILE", 0, "Output stats to FILE" },
   { 0 }
 };
@@ -32,6 +34,7 @@ struct arguments {
   size_t payload_size;
   int tracepoints_per_request;
   float trigger_probability;
+  uint64_t duration;
   char* output_file;
   char* process_name;
 };
@@ -81,8 +84,49 @@ static error_t parse_opt (int key, char *arg, struct argp_state *state) {
   return 0;
 }
 
-
 static struct argp argp = { options, parse_opt, args_doc, doc };
+
+void init_hindsight_client(struct arguments *arguments) {
+    HindsightConfig conf;
+    conf.pool_capacity = arguments->buffer_count;
+    conf.buffer_size = arguments->buffer_size;
+    conf.breadcrumbs_capacity = conf.pool_capacity;
+    conf.triggers_capacity = conf.pool_capacity;
+    conf.address = malloc(32 * sizeof(char));
+
+    hindsight_init_with_config(arguments->process_name, conf);
+}
+
+typedef struct client_args {
+    int client_id;
+    struct arguments* arguments;
+
+} client_args;
+
+void client_thread_main(int client_id, struct arguments *arguments) {
+    printf("Client %d started\n", client_id);
+}
+
+void* run_client_thread(void *vargp) {
+    client_args* args = (client_args*) vargp;
+    client_thread_main(args->client_id, args->arguments);
+    return 0;
+}
+
+void run_clients(struct arguments *arguments) {
+    printf("Running clients\n");
+    pthread_t threads[arguments->num_threads];
+    client_args args[arguments->num_threads];
+    for (int i = 0; i < arguments->num_threads; i++) {
+        args[i].client_id = i;
+        args[i].arguments = arguments;
+        pthread_create(&threads[i], NULL, run_client_thread, (void*) &args[i]);
+    }
+    for (int i = 0; i < arguments->num_threads; i++) {
+        pthread_join(threads[i], NULL);
+    }
+}
+
 
 int main (int argc, char **argv) {
   struct arguments arguments;
@@ -94,6 +138,7 @@ int main (int argc, char **argv) {
   arguments.payload_size = 400;
   arguments.tracepoints_per_request = 100;
   arguments.trigger_probability = 0;
+  arguments.duration = 0;
 
 
   arguments.process_name = 0;
@@ -103,12 +148,17 @@ int main (int argc, char **argv) {
      be reflected in arguments. */
   argp_parse (&argp, argc, argv, 0, 0, &arguments);
 
-  printf("name=%s\nbuffer_size=%ld\nbuffer_count=%ld\nnum_threads=%d\npayload_size=%ld\ntp_per=%d\ntrigger=%.3f\n",
-  			arguments.process_name,
-  			arguments.buffer_size, arguments.buffer_count,
-  			arguments.num_threads, arguments.payload_size,
-  			arguments.tracepoints_per_request,
-  			arguments.trigger_probability);
+  printf("name=%s\nbuffer_size=%ld\nbuffer_count=%ld\nnum_threads=%d\npayload_size=%ld\ntp_per=%d\ntrigger=%.3f\n-------\n",
+            arguments.process_name,
+            arguments.buffer_size, arguments.buffer_count,
+            arguments.num_threads, arguments.payload_size,
+            arguments.tracepoints_per_request,
+            arguments.trigger_probability);
+
+  init_hindsight_client(&arguments);
+  printf("------\n");
+
+  run_clients(&arguments);
 
   exit (0);
 }
