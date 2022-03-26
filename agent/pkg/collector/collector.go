68ec34b5990a4490f8a04e5982639e2a32471a6b
Jonathan Mace <jonathan.c.mace@gmail.com>
2022-03-26 21:04:51 +0100

    Write trace data to disk

--- agent/pkg/collector/collector.go
+++ agent/pkg/collector/collector.go
@@ -4,18 +4,24 @@ import (
 	"context"
 	"encoding/binary"
 	"fmt"
+	"log"
 	"net"
+	"os"
 	"time"
 
 	"github.com/geraldleizhang/hindsight/agent/pkg/memory"
 )
 
 type Collector struct {
-	port string
+	tracefile string
+	port      string
+	incoming  chan *ReceivedBuffer
 }
 
-func (c *Collector) Init(port string) {
+func (c *Collector) Init(port string, tracefile string) {
+	c.tracefile = tracefile
 	c.port = port
+	c.incoming = make(chan *ReceivedBuffer, 1000)
 }
 
 func (c *Collector) Run(ctx context.Context) {
@@ -24,6 +30,14 @@ func (c *Collector) Run(ctx context.Context) {
 	if err != nil {
 		fmt.Println("Error listening on port", c.port, err)
 		return
+	}
+
+	if c.tracefile != "" {
+		log.Println("Writing trace data to", c.tracefile)
+		go c.fileWriter(c.tracefile)
+	} else {
+		log.Println("Not writing trace data to disk")
+		go c.printer()
 	}
 
 	for {
@@ -61,8 +75,13 @@ func readLengthPrefixed(conn net.Conn) (buf []byte, err error) {
 }
 
 func (c *Collector) handleConnection(conn net.Conn) {
-	next_print := time.Now()
-	count := 0
+	buf, err := readLengthPrefixed(conn)
+	if err != nil {
+		fmt.Println("Error receiving handshake from new agent connection", err)
+		return
+	}
+	agent_addr := string(buf)
+	fmt.Println("New connection from", agent_addr)
 
 	for {
 		buf, err := readLengthPrefixed(conn)
@@ -70,16 +89,71 @@ func (c *Collector) handleConnection(conn net.Conn) {
 			fmt.Println("Error in handleConnection receiving next buffer", err)
 			return
 		}
-		memory.ExtractBufferHeader(buf)
-		// header := memory.ExtractBufferHeader(buf)
-		// fmt.Println("Read buf of size", len(buf), header)
-		count += len(buf)
+		if len(buf) >= 32 { // 32 is size of buffer header
+			header := memory.ExtractBufferHeader(buf)
+			// header := memory.ExtractBufferHeader(buf)
+			// fmt.Println("Read buf of size", len(buf), header)
+			// fmt.Println("Buffer ID was", header.Buffer_id, "Prev Buffer ID was", header.Prev_buffer_id)
+			var r ReceivedBuffer
+			r.trace_id = header.Trace_id
+			r.source_agent = agent_addr
+			r.buffer = buf
+			c.incoming <- &r
+		}
+	}
+}
 
-		if next_print.Before(time.Now()) {
-			tput := float32(count) / float32(1024*1024)
-			fmt.Printf("%.2f MB/s\n", tput)
-			count = 0
-			next_print = next_print.Add(time.Duration(1) * time.Second)
+func (c *Collector) fileWriter(filename string) {
+	f, err := os.Create(filename)
+	if err != nil {
+		fmt.Println("Error creating /local/buffers.out: ", err)
+		return
+	}
+	ticker := time.NewTicker(1 * time.Second)
+	count := 0
+	last_report := time.Now()
+	for {
+		select {
+		case <-ticker.C:
+			{
+				now := time.Now()
+				interval := now.Sub(last_report)
+				last_report = now
+				tput := (float64(count) / interval.Seconds()) / (1024 * 1024)
+				log.Printf("%.2f MB/s\n", tput)
+				count = 0
+			}
+		case r := <-c.incoming:
+			{
+				count += len(r.buffer)
+				err = r.WriteToFile(f)
+				if err != nil {
+					fmt.Println("Error writing buffer to file: ", err)
+				}
+			}
+		}
+	}
+}
+
+func (c *Collector) printer() {
+	ticker := time.NewTicker(1 * time.Second)
+	count := 0
+	last_report := time.Now()
+	for {
+		select {
+		case <-ticker.C:
+			{
+				now := time.Now()
+				interval := now.Sub(last_report)
+				last_report = now
+				tput := (float64(count) / interval.Seconds()) / (1024 * 1024)
+				log.Printf("%.2f MB/s\n", tput)
+				count = 0
+			}
+		case r := <-c.incoming:
+			{
+				count += len(r.buffer)
+			}
 		}
 	}
 }
